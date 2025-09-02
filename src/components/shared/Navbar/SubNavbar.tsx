4fb0336112a40343acda6e5b185524547cccad8f
mrashed21 <rashedjaman768@gmail.com>
2025-09-02 20:16:43 +0600

    add main navbar

--- src/components/shared/Navbar/SubNavbar.tsx
+++ src/components/shared/Navbar/SubNavbar.tsx
@@ -2,7 +2,9 @@
 
 import Container from "@/components/Reusable/Container/Container";
 import SearchInput from "@/components/Reusable/SearchInput/SearchInput";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaGift } from "react-icons/fa";
+import { IoCartOutline } from "react-icons/io5";
+import { PiNoteDuotone } from "react-icons/pi";
 
 interface SubNavbarProps {
   setIsMobileMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -17,7 +19,7 @@ const SubNavbar: React.FC<SubNavbarProps> = ({ setIsMobileMenuOpen }) => {
           <h1 className="text-xl font-semibold ">Decent</h1>
         </div>
         {/* search section */}
-        <div className="w-full xl:max-w-3xl">
+        <div className="w-full xl:max-w-xl">
           <SearchInput
             placeholder="Search for products..."
             className="bg-primary text-secondary"
@@ -31,11 +33,27 @@ const SubNavbar: React.FC<SubNavbarProps> = ({ setIsMobileMenuOpen }) => {
           />
         </div>
         {/* for xl screen */}
-        <section className="hidden xl:flex items-center space-x-3">
-          <button>Offter</button>
-          <button>Prime Order</button>
-          <button>Cart</button>
-          <button>Login</button>
+        <section className="hidden xl:flex items-center space-x-5">
+          <button className="relative px-7 py-2 rounded-full bg-[#111] flex items-center justify-center overflow-hidden">
+            <span className="bg-gradient-to-r from-yellow-200 to-yellow-500 bg-clip-text text-transparent font-medium text-sm">
+              OFFER
+            </span>
+            <span className="absolute -top-1 right-3 text-yellow-400 text-lg">
+              <FaGift />
+            </span>
+          </button>
+
+          <button className="px-5 py-2 border border-tertiary rounded-xl text-tertiary flex items-center gap-1 text-sm cursor-pointer">
+            <PiNoteDuotone size={16} /> Prime Order
+          </button>
+
+          <button className="px-5 py-2 border border-tertiary rounded-xl text-tertiary flex items-center gap-1 text-sm cursor-pointer">
+            <IoCartOutline size={16} /> Cart
+          </button>
+
+          <button className="px-5 py-2 border border-tertiary rounded-xl text-tertiary flex items-center gap-1 text-sm cursor-pointer">
+            Login
+          </button>
         </section>
       </Container>
     </section>
--- src/components/shared/Navbar/TopNavbar.tsx
+++ src/components/shared/Navbar/TopNavbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Container from "@/components/Reusable/Container/Container";
+import Link from "next/link";
 import { FiPhoneCall } from "react-icons/fi";
 
 const TopNavbar = () => {
@@ -7,16 +8,16 @@ const TopNavbar = () => {
     <section className="hidden lg:flex">
       <Container>
         <div className="py-2.5 flex justify-between text-sm">
-          <span className="flex items-center gap-1">
+          <Link href={"tel:+8801712345678"} className="flex items-center gap-1">
             <FiPhoneCall /> +8801712345678
-          </span>
+          </Link>
 
           <div className="space-x-3 uppercase text-xs font-medium">
-            <span className=""> Order Tracking</span>
-            <span className="">Gift</span>
-            <span className="">Blogs</span>
-            <span className="">EMI Policy</span>
-            <span className="">Store Location</span>
+            <span className="cursor-pointer"> Order Tracking</span>
+            <span className="cursor-pointer">Gift</span>
+            <span className="cursor-pointer">Blogs</span>
+            <span className="cursor-pointer">EMI Policy</span>
+            <span className="cursor-pointer">Store Location</span>
           </div>
         </div>
       </Container>
