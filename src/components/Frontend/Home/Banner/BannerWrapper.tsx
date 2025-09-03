704eca7c8669b6a792814075360ba364b5681f23
mrashed21 <rashedjaman768@gmail.com>
2025-09-03 11:32:39 +0600

    complate banner section

--- src/components/Frontend/Home/Banner/BannerWrapper.tsx
+++ src/components/Frontend/Home/Banner/BannerWrapper.tsx
@@ -1,25 +1,3 @@
-// import Container from "@/components/Reusable/Container/Container";
-// import MainBanner from "./MainBanner";
-// import SideBanner from "./SideBanner";
-
-// const BannerWrapper = () => {
-//   return (
-//     <Container>
-//       <section className="grid grid-cols-1 xl:grid-cols-3 py-2 gap-5">
-//         {/* main banner */}
-//         <section className="col-span-1 xl:col-span-2">
-//           <MainBanner />
-//         </section>
-//         {/* side banner */}
-
-//         <SideBanner />
-//       </section>
-//     </Container>
-//   );
-// };
-
-// export default BannerWrapper;
-
 import Container from "@/components/Reusable/Container/Container";
 import MainBanner from "./MainBanner";
 import SideBanner from "./SideBanner";
--- src/components/Frontend/Home/Banner/SideBanner.tsx
+++ src/components/Frontend/Home/Banner/SideBanner.tsx
@@ -1,27 +1,3 @@
-// const SideBanner = () => {
-//   return (
-//     <div className="grid grid-cols-2 xl:grid-cols-1 gap-3">
-//       <div className="min-w-36 min-h-36 lg:max-h-[350px] !lg:w-full">
-//         <img
-//           src="https://dazzle.com.bd/_next/image?url=https%3A%2F%2Fdazzle.sgp1.cdn.digitaloceanspaces.com%2F54748%2FIPHONE-16-series.jpg&w=1920&q=75"
-//           alt="banner"
-//           className="rounded-xl lg:rounded-2xlw-full h-full "
-//         />
-//       </div>
-//       <div className="min-w-36 min-h-36 lg:max-h-[350px] !lg:w-full">
-//         <img
-//           src="https://dazzle.com.bd/_next/image?url=https%3A%2F%2Fdazzle.sgp1.cdn.digitaloceanspaces.com%2F54748%2FIPHONE-16-series.jpg&w=1920&q=75"
-//           alt="banner"
-//           className="rounded-xl lg:rounded-2xl w-full h-full "
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SideBanner;
-
-
 const SideBanner = () => {
   return (
     <div className="grid grid-cols-2 xl:grid-cols-1 gap-3">
